9b6439e04356c1d94af60b889e3003ddbe458164
Novák György <gnovak@loginet.hu>
2023-11-25 16:06:44 +0100

    [feature] Async Storage

--- CsodApp/stacks/CsodAppProgressStack.js
+++ CsodApp/stacks/CsodAppProgressStack.js
@@ -1,10 +1,40 @@
 import Menu from '../components/Menu';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { useState, useEffect } from 'react';
+
+/* region Progress */
+import AsyncStorage from '@react-native-async-storage/async-storage';
+/* endregion */
 
 export default function CsodAppProgressStack() {
+  /* region Progress */
+  const [progress, setProgress] = useState(0);
+
+  setProgressToAsyncStorage = async (progress) => {
+    try {
+      await AsyncStorage.setItem(
+        'progress',
+        `${progress+4}`
+      );
+      setProgress(progress);
+    } catch (error) {}
+  };
+
+  getProgressFromAsyncStorage = async () => {
+    try {
+      const progress = await AsyncStorage.getItem('progress');
+      if (progress !== null) {
+        setProgress(parseInt(progress));
+      }
+    } catch (error) {}
+  };
+  /* endregion */
+
   return (
     <View style={styles.container}>
-      <Text>ListenStack</Text>
+      <Button title="Add+Set" onPress={() => { setProgressToAsyncStorage(progress+1) }}></Button>
+      <Text>{progress}</Text>
+      <Button title="Get" onPress={() => { getProgressFromAsyncStorage() }}></Button>
       <Menu />
     </View>
   );
